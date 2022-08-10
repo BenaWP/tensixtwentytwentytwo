9828da86b05a52055f2eb60017a03c616bb7bfcd
yvonbenahita <yvonbenahita@gmail.com>
2022-08-10 14:35:24 +0300

    Fix description + translate searchform

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
         <input type="text" value="" name="s" id="s" placeholder="<?php _e( 'Tapez ici...', 'tensixtwentytwentytwo' ); ?>" />
-        <input type="submit" id="searchsubmit" value="Rechercher" />
+        <input type="submit" id="searchsubmit" value="<?php  esc_attr_e('Rechercher', 'tensixtwentytwentytwo'); ?>" />
     </div>
 </form>
