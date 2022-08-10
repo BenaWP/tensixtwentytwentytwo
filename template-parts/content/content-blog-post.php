913afc1c67e7439a073073150265c0fb753aa3dd
yvonbenahita <yvonbenahita@gmail.com>
2022-08-10 17:26:08 +0300

    Translating content blog post

--- template-parts/content/content-blog-post.php
+++ template-parts/content/content-blog-post.php
@@ -2,26 +2,27 @@
     <header class="content-header">
         <div class="meta mb-3">
             <span class="date"> 
-                <?php 
-                    $humain_time = human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) );
-                    printf( 'Publié il y a %s',  $humain_time);
+                <?php
+                $humain_time = human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) );
+                printf( 'Publié il y a %s', $humain_time );
                 ?>
             </span>
-            <?php 
-                the_tags( '<span class="tag"><i class="fa fa-tag"></i> ', ' </span><span class="tag"><i class="fa fa-tag"></i> ', '</span>' );
-            ?>
-            <?php 
-                the_category( '<span class="tag"><i class="fa fa-tag"></i> ', ' </span><span class="tag"><i class="fa fa-tag"></i> ', '</span>' ); 
-            ?>
-            <span class="comment"><a href="#comments"><i class='fa fa-comment'></i> <?php comments_number(); ?> </a></span>
+			<?php
+			the_tags( '<span class="tag"><i class="fa fa-tag"></i> ', ' </span><span class="tag"><i class="fa fa-tag"></i> ', '</span>' );
+			?>
+			<?php
+			the_category( '<span class="tag"><i class="fa fa-tag"></i> ', ' </span><span class="tag"><i class="fa fa-tag"></i> ', '</span>' );
+			?>
+            <span class="comment"><a href="#comments"><i
+                            class='fa fa-comment'></i> <?php comments_number(); ?> </a></span>
         </div>
     </header>
-    <?php
-    the_content();
-    ?>
+	<?php
+	the_content();
+	?>
 
     <!-- comments section -->
-    <?php 
-        comments_template();
-    ?>
+	<?php
+	comments_template();
+	?>
 </div>
