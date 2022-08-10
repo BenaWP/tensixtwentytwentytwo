026f4375726d8cf9fcc720c3ef0e2754a292c0dc
yvonbenahita <yvonbenahita@gmail.com>
2022-08-10 17:23:58 +0300

    Translating content-blog-home

--- template-parts/content/content-blog-home.php
+++ template-parts/content/content-blog-home.php
@@ -2,7 +2,7 @@
     <div class="post mb-5">
         <div class="media">
             <?php 
-                if( has_post_thumbnail($post->ID) ) :
+                if( has_post_thumbnail( $post->ID ) ) :
                     ?>
                     <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php  the_post_thumbnail_url( 'thumbnail' );?>" alt="image">
                     <?php
@@ -15,7 +15,7 @@
                 <h3 class="title mb-1"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h3>
                 <div class="meta mb-1"><span class="date"> <?php the_date(); ?> </span><span class="comment"><a href="#"> <?php comments_number(); ?> </a></span></div>
                 <div class="intro"> <?php the_excerpt(); ?> </div>
-                <a class="more-link" href="<?php the_permalink(); ?>">Read more &rarr;</a>
+                <a class="more-link" href="<?php the_permalink(); ?>"><?php esc_html_e( 'Lire la suite &rarr;', 'tensixtwentytwentytwo' ); ?></a>
             </div>
             <!--//media-body-->
         </div>
